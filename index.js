31d8ca6932782f6d2e1f9c64ce1d531cd75c2d90
BugHunter-X <askzenai@gmail.com>
2025-02-10 20:46:07 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,28 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* Solution: Removed incorrect usage of params */}
+    </div>
+  );
+}
+
+export default About; 
+```
